38ce0d1f6a724a55f9d410fb0d8db522b90a13de
Arnab <senapati.arnab@gmail.com>
2015-01-04 12:14:04 +0530

    New Icon

--- gen/com/example/jstock_droid/R.java
+++ gen/com/example/jstock_droid/R.java
@@ -43,6 +43,7 @@ public final class R {
         public static final int title_section1=0x7f050001;
         public static final int title_section2=0x7f050002;
         public static final int title_section3=0x7f050003;
+        public static final int welcome_string=0x7f050006;
     }
     public static final class style {
         /** 
